bbd23c5d566ccdb1850af5f6c5578a7a753a6c18
Nikolai Karastelev <n.karastelev@credinform.ru>
2020-03-11 20:00:27 +0300

    Add calcAreaOfTriangle callbacks implementations

--- src/solution.js
+++ src/solution.js
@@ -1,10 +1,10 @@
 
 (function (window) {
-
-  const { subtract } = window.Homework;
+  const { subtract, multiply, less } = window.Homework;
 
   /**
    * Посчитать площадь треугольника
+   * @description С использованием callbacks
    * @param {number} x1
    * @param {number} y1
    * @param {number} x2
@@ -13,10 +13,26 @@
    * @param {number} y3
    * @param {Function} cb
    */
-  function calcAareaOfTriangle(x1, y1, x2, y2, x3, y3, cb) {
-    setTimeout(cb, 600);
+  function calcAreaOfTriangle_cb(x1, y1, x2, y2, x3, y3, cb) {
+    subtract(x1, x3, a => {
+      subtract(y1, y3, b => {
+        subtract(x2, x3, c => {
+          subtract(y2, y3, d => {
+            multiply(a, d, e => {
+              multiply(b, c, f => {
+                subtract(e, f, g => {
+                  less(g, 0, isNegative => {
+                    multiply(g, isNegative ? -0.5 : 0.5, cb);
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
   }
 
-  window.calcAareaOfTriangle = calcAareaOfTriangle;
+  window.calcAreaOfTriangle_cb = calcAreaOfTriangle_cb;
 
 })(window);
--- src/solution.test.js
+++ src/solution.test.js
@@ -1,27 +1,39 @@
 describe('Домашнее задание ШРИ по теме “Асинхронность”', function() {
-  describe('Вариант 4. Посчитать площадь треугольника.', function() {
+  describe('Вариант 4. Посчитать площадь треугольника (callback)', function() {
     it('возвращает площать треугольника 1', function(done) {
       const x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0;
 
-      calcAareaOfTriangle(x1, y1, x2, y2, x3, y3, (result) => {
-        chai.assert.equal(result, 0);
-        done();
+      calcAreaOfTriangle_cb(x1, y1, x2, y2, x3, y3, area => {
+        try {
+          chai.assert.equal(area, 0);
+          done();
+        } catch (error) {
+          done(error);
+        }
       });
     });
     it('возвращает площать треугольника 2', function(done) {
       const x1 = 1, y1 = 1, x2 = 2, y2 = 5, x3 = 5, y3 = 3;
 
-      calcAareaOfTriangle(x1, y1, x2, y2, x3, y3, (result) => {
-        chai.assert.equal(result, 7);
-        done();
+      calcAreaOfTriangle_cb(x1, y1, x2, y2, x3, y3, area => {
+        try {
+          chai.assert.equal(area, 7);
+          done();
+        } catch (error) {
+          done(error);
+        }
       });
     });
     it('возвращает площать треугольника 3', function(done) {
       const x1 = 0, y1 = 0, x2 = 10, y2 = 3, x3 = 4, y3 = 8;
 
-      calcAareaOfTriangle(x1, y1, x2, y2, x3, y3, (result) => {
-        chai.assert.equal(result, 34);
-        done();
+      calcAreaOfTriangle_cb(x1, y1, x2, y2, x3, y3, area => {
+        try {
+          chai.assert.equal(area, 34);
+          done();
+        } catch (error) {
+          done(error);
+        }
       });
     });
   });
