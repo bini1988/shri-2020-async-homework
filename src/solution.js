ee883ade55ada1f7eed30baf2e4637bc42d65fd0
Nikolai <rus.nick@mail.ru>
2020-03-11 21:41:43 +0300

    Add calcAreaOfTriangle_promise solution

--- src/solution.js
+++ src/solution.js
@@ -1,6 +1,10 @@
 
 (function (window) {
-  const { subtract, multiply, less } = window.Homework;
+  function promisify(fn) {
+    return (...args) => new Promise(
+      resolve => fn(...args, resolve)
+    );
+  }
 
   /**
    * Посчитать площадь треугольника
@@ -14,6 +18,8 @@
    * @param {Function} cb
    */
   function calcAreaOfTriangle_cb(x1, y1, x2, y2, x3, y3, cb) {
+    const { subtract, multiply, less } = window.Homework;
+
     subtract(x1, x3, a => {
       subtract(y1, y3, b => {
         subtract(x2, x3, c => {
@@ -33,6 +39,37 @@
     });
   }
 
+  /**
+   * Посчитать площадь треугольника
+   * @description С использованием Promise
+   * @param {number} x1
+   * @param {number} y1
+   * @param {number} x2
+   * @param {number} y2
+   * @param {number} x3
+   * @param {number} y3
+   */
+  function calcAreaOfTriangle_promise(x1, y1, x2, y2, x3, y3) {
+    const subtract = promisify(window.Homework.subtract);
+    const multiply = promisify(window.Homework.multiply);
+    const less = promisify(window.Homework.less);
+
+    return Promise.all([
+      subtract(x1, x3),
+      subtract(y1, y3),
+      subtract(x2, x3),
+      subtract(y2, y3),
+    ]).then(([a, b, c, d]) =>
+      Promise.all([
+        multiply(a, d),
+        multiply(b, c),
+      ]))
+    .then(([e, f]) => subtract(e, f))
+    .then(g => Promise.all([less(g, 0), g]))
+    .then(([isNegative, g]) => multiply(g, isNegative ? -0.5 : 0.5));
+  }
+
   window.calcAreaOfTriangle_cb = calcAreaOfTriangle_cb;
+  window.calcAreaOfTriangle_promise = calcAreaOfTriangle_promise;
 
 })(window);
--- src/solution.test.js
+++ src/solution.test.js
@@ -37,4 +37,24 @@ describe('Домашнее задание ШРИ по теме “Асинхро
       });
     });
   });
+  describe('Вариант 4. Посчитать площадь треугольника (promise)', function() {
+    it('возвращает площать треугольника 1', function() {
+      const x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0;
+
+      return calcAreaOfTriangle_promise(x1, y1, x2, y2, x3, y3)
+        .then(area => { chai.assert.equal(area, 0) });
+    });
+    it('возвращает площать треугольника 2', function() {
+      const x1 = 1, y1 = 1, x2 = 2, y2 = 5, x3 = 5, y3 = 3;
+
+      return calcAreaOfTriangle_promise(x1, y1, x2, y2, x3, y3)
+        .then(area => { chai.assert.equal(area, 7) });
+    });
+    it('возвращает площать треугольника 3', function() {
+      const x1 = 0, y1 = 0, x2 = 10, y2 = 3, x3 = 4, y3 = 8;
+
+      return calcAreaOfTriangle_promise(x1, y1, x2, y2, x3, y3)
+        .then(area => { chai.assert.equal(area, 34) });
+    });
+  });
 });
